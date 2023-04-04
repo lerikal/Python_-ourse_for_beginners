e7ed829e209035decf536d0cefb05e7cfdc226ca
lerikal <129775460+lerikal@users.noreply.github.com>
2023-04-04 14:04:43 +0200

    Update Ch4Ex1.py

--- Stepik_Python/Ch4Ex1.py
+++ Stepik_Python/Ch4Ex1.py
@@ -1,5 +1,15 @@
 def update_dictionary(d, key, value):
     if key in d.keys():
-        d[key] = [value]
-    elif key not in d.keys():
+        d.get(key).append(value)
+    elif key*2 in d.keys():
+        d.get(key*2).append(value)
+    else:
         d[2 * key] = [value]
+
+d = {}
+print(update_dictionary(d, 1, -1))  # None
+print(d)                            # {2: [-1]}
+update_dictionary(d, 2, -2)
+print(d)                            # {2: [-1, -2]}
+update_dictionary(d, 1, -3)
+print(d)                            # {2: [-1, -2, -3]}
